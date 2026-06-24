c8837148b680ddf8bb3d991249ec9e2913eb4ab7
bizmedia1 <victorisrael539@gmail.com>
2026-06-24 22:25:50 +0100

    Update vibesreel-checkoutmvn.js

--- api/vibesreel-checkoutmvn.js
+++ api/vibesreel-checkoutmvn.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -7,40 +8,13 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  try {
-    const response = await fetch("https://mevonpay.com.ng/V1/createtempva", {
-      method: "POST",
-      headers: {
-        "Authorization": process.env.MEVON_SECRET_KEY, // ⚠️ NO Bearer here for this endpoint
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        fname: "John",
-        lname: "Doe"
-      })
-    });
+  return res.status(200).json({
+    test: "Victor",
+    account_number: "1111111111",
+    account_name: "TEST USER",
+    bank_name: "Paga",
+    amount: 14000,
+    reference: "ABC123"
+  });
 
-    const text = await response.text();
-
-    let parsed;
-    try {
-      const result = JSON.parse(text);
-      parsed = result.raw ? JSON.parse(result.raw) : result;
-    } catch {
-      return res.status(500).json({ error: text });
-    }
-
-    return res.status(200).json({
-  test: "Victor",
-  account_number: "1111111111",
-  account_name: "TEST USER",
-  bank_name: "Paga",
-  amount: 14000,
-  reference: "ABC123"
-});
-  } catch (err) {
-    return res.status(500).json({
-      error: err.message
-    });
-  }
 }
